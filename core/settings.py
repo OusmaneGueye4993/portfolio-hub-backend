7e0b3550dcd0322936377198315bb8acee090220
OusmaneGueye4993 <ousmaneg104@gmail.com>
2026-07-01 16:41:13 +0000

    Fix: passage au port SSL 465 pour contourner le blocage réseau de Render

--- core/settings.py
+++ core/settings.py
@@ -147,20 +147,35 @@ CORS_ALLOWED_ORIGINS = [
     "https://ousmanegueye.vercel.app",
 ]
 
-# Remplacez la ligne existante par celle-ci :
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# # Remplacez la ligne existante par celle-ci :
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='ousmaneg104@gmail.com')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='mtfqcixpabtnqjvi')
+
+# DEFAULT_FROM_EMAIL = f"Portfolio Contact <{EMAIL_HOST_USER}>"
+# ADMIN_EMAIL = EMAIL_HOST_USER
+
+
+
+# Configuration du serveur de messagerie SMTP (Gmail via SSL Port 465)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+# On change de port et on force SSL à True au lieu de TLS
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='ousmaneg104@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='mtfqcixpabtnqjvi')
 
 DEFAULT_FROM_EMAIL = f"Portfolio Contact <{EMAIL_HOST_USER}>"
 ADMIN_EMAIL = EMAIL_HOST_USER
-
-
 
 
 
